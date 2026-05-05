ac533c807d4e6da345e7a4161e3a2be48b60394d
YOGA AP <macbook@MacBook-Pro-YogaAP.local>
2026-05-06 01:56:32 +0700

    Critical Fix: Adjust artisan and composer paths for Vercel root

--- backend/public/index.php
+++ backend/public/index.php
@@ -12,7 +12,12 @@ try {
         require $maintenance;
     }
 
-    require __DIR__.'/../../vendor/autoload.php';
+    // Register the Composer autoloader...
+    if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+        require __DIR__.'/../vendor/autoload.php';
+    } else {
+        require __DIR__.'/../../vendor/autoload.php';
+    }
 
     $app = require_once __DIR__.'/../bootstrap/app.php';
 
